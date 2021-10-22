adac1f07d92fff0b1785113bdeaa2dacaa06b8f6
Ahmed hosni <Ahmedhosni.contact@gmail.com>
2021-10-22 20:10:44 +0000

    adding a nullable field

--- musiques/models.py
+++ musiques/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Morceau(models.Model):
     titre = models.CharField(max_length=64)
     artiste = models.CharField(max_length=64)
+    date_sortie = models.DateField(null=True)
 
     def __str__(self):
         return '{self.titre} (({self.artiste})'.format(self=self)
