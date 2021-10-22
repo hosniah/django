00db5f05d1585ada76dce2bb2b8c9167aa8801b9
Ahmed hosni <Ahmedhosni.contact@gmail.com>
2021-10-22 10:48:01 +0000

    Fixing unit tests

--- musiques/urls.py
+++ musiques/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import morceau_detail
 
 app_name = 'musiques'
 
-urlpatterns = []
+urlpatterns = [
+    path('<int:pk>', morceau_detail, name='morceau-detail')
+]
