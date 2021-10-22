b6ec69e13f097a732628462e4d6764c48f4f569f
Ahmed hosni <Ahmedhosni.contact@gmail.com>
2021-10-22 15:21:14 +0000

    Add list view

--- musiques/urls.py
+++ musiques/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MorceauDetailView
+from .views import MorceauDetailView, MorceauList
 
 app_name = 'musiques'
 
 
 urlpatterns = [
 #    path('<int:pk>', morceau_detail, name='morceau-detail')
-      path('<int:pk>',MorceauDetailView.as_view(), name='morceau-detail')
+     path('<int:pk>',MorceauDetailView.as_view(), name='morceau-detail'),
+     path('', MorceauList.as_view(), name='morceau_list'),
 ]
--- musiques/views.py
+++ musiques/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Morceau
 
 # Create your views here.
 
 class MorceauDetailView(DetailView):
+    model = Morceau
+
+class MorceauList(ListView):
     model = Morceau
