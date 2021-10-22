f0cecdaaede9b419bce5b3c5ed779251ade47e8d
Ahmed hosni <Ahmedhosni.contact@gmail.com>
2021-10-22 21:01:13 +0000

    update model

--- musiques/models.py
+++ musiques/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Artiste(models.Model):
+    nom = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.nom
+
 class Morceau(models.Model):
     titre = models.CharField(max_length=64)
     artiste = models.CharField(max_length=64)
